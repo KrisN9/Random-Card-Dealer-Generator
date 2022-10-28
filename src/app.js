0de1cdc34d819ab39f4a56b2366d623bfcf366ec
KrisN9 <kristinank992@gmail.com>
2022-10-28 19:38:01 +0000

    Modificaciones del viernes 28

--- src/app.js
+++ src/app.js
@@ -4,8 +4,12 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
+let lista = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let num = document.querySelector("#numeros");
+  let lista = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  let valor = Math.floor(Math.random() * lista.length);
+  num.innerHTML = valor;
+  console.log(valor);
 };
