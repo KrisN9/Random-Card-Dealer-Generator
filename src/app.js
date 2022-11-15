6ef6e11e10cde2e8a8a9fff82b82f49ede7a2a2c
KrisN9 <kristinank992@gmail.com>
2022-11-15 23:32:55 +0000

    Pendiente de resolver

--- src/app.js
+++ src/app.js
@@ -2,10 +2,11 @@
 import "bootstrap";
 import "./style.css";
 
-let lista = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
-window.onload = function() {
+let suits = ["♦", "♥", "♠", "♣"];
+let numbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+window.onload = randomCard => {
   //write your code here
-  let num = document.querySelector("#numeros");
-  let lista = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
-  let valor = Math.floor(Math.random() * lista.length);
+  document.getElementById("#symbol-top, #symbol-bottom");
+  document.getElementById("#number");
 };
