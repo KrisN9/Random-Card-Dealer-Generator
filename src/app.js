a9703df7724f7bbf5e349181063feb13980ff5d1
KrisN9 <kristinank992@gmail.com>
2022-11-15 19:30:52 +0000

    Carta inicial con Bootstrap

--- src/app.js
+++ src/app.js
@@ -2,8 +2,6 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 let lista = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
 window.onload = function() {
   //write your code here
