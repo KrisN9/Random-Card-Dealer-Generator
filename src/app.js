ea39c40d9d5e539368e973436f54d01063a06fd8
KrisN9 <kristinank992@gmail.com>
2022-11-16 23:42:23 +0000

    modificaciones de estilo adicionales

--- src/app.js
+++ src/app.js
@@ -7,20 +7,5 @@ let numbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
 window.onload = () => {
   //write your code here
-  document.querySelector(".card").addEventListener("click", event => {});
+  console.log("Everything is working");
 };
-
-const deckBuilder = () => {
-  const card = [];
-  for (let s = 0; s < suits.length; s++) {
-    for (let n = 0; n < numbers.length; n++) {
-      const number = numbers[n];
-      const suit = suits[s];
-      card.push({ number, suit });
-    }
-  }
-  return card;
-};
-const randomCard = card => {};
-const card = deckBuilder();
-randomCard(card);
