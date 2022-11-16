ab9b5fe4b1d66c6d452bb5a438c15632ca05a374
KrisN9 <kristinank992@gmail.com>
2022-11-16 20:58:02 +0000

    haciendo funciones javascript

--- src/app.js
+++ src/app.js
@@ -9,3 +9,18 @@ window.onload = () => {
   //write your code here
   document.querySelector(".card").addEventListener("click", event => {});
 };
+
+const deckBuilder = () => {
+  const card = [];
+  for (let s = 0; s < suits.length; s++) {
+    for (let n = 0; n < numbers.length; n++) {
+      const number = numbers[n];
+      const suit = suits[s];
+      card.push({ number, suit });
+    }
+  }
+  return card;
+};
+const randomCard = card => {};
+const card = deckBuilder();
+randomCard(card);
