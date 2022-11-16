b957d5c6b7969c48798c2a5cf92a81cbbcbc7f98
KrisN9 <kristinank992@gmail.com>
2022-11-16 19:58:56 +0000

    pendiente funcion javascript

--- src/app.js
+++ src/app.js
@@ -5,8 +5,7 @@ import "./style.css";
 let suits = ["♦", "♥", "♠", "♣"];
 let numbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
-window.onload = randomCard => {
+window.onload = () => {
   //write your code here
-  document.getElementById("#symbol-top, #symbol-bottom");
-  document.getElementById("#number");
+  document.querySelector(".card").addEventListener("click", event => {});
 };
