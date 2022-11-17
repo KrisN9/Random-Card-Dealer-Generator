a80a557c12e4d7c8336811eebbb7e43fc8268765
KrisN9 <kristinank992@gmail.com>
2022-11-17 00:56:13 +0000

    numero aleatorio en lugar erroneo

--- src/app.js
+++ src/app.js
@@ -7,5 +7,16 @@ let numbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
 window.onload = () => {
   //write your code here
-  console.log("Everything is working");
+  document.querySelector("h4").innerHTML = generateRandomSuit();
+  document.querySelector(".card").innerHTML = generateRandomNumber();
+};
+
+const generateRandomNumber = () => {
+  let randomNumber = Math.floor(Math.random() * numbers.length);
+  return numbers[randomNumber];
+};
+
+const generateRandomSuit = () => {
+  let randomSuit = Math.floor(Math.random() * suits.length);
+  return suits[randomSuit];
 };
