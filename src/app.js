65ad7fcbd00a07f8a8b69935555ee80e8197b0ec
KrisN9 <kristinank992@gmail.com>
2022-11-17 18:43:44 +0000

    Se genera número aleatorio en la carta

--- src/app.js
+++ src/app.js
@@ -7,8 +7,7 @@ let numbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
 window.onload = () => {
   //write your code here
-  document.querySelector("h4").innerHTML = generateRandomSuit();
-  document.querySelector(".card").innerHTML = generateRandomNumber();
+  document.querySelector("p").innerHTML = generateRandomNumber();
 };
 
 const generateRandomNumber = () => {
