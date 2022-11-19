8a0723521dbc948e57dd2fd4a0bb30eccc030ac6
KrisN9 <kristinank992@gmail.com>
2022-11-19 16:17:32 +0000

    carta aleatoria para entregar

--- src/app.js
+++ src/app.js
@@ -2,21 +2,25 @@
 import "bootstrap";
 import "./style.css";
 
-let suits = ["♦", "♥", "♠", "♣"];
+let suits = ["&#9829;", "&#9830;", "&#9824;", "&#9827;"];
 let numbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
 window.onload = () => {
   //write your code here
   document.querySelector("p").innerHTML = generateRandomNumber();
-  document.querySelectorAll("h3").textContent = generateRandomSuit();
+  generateRandomSuit();
 };
-
+const Suits = document.querySelectorAll(".card-suit");
 const generateRandomNumber = () => {
   let randomNumber = Math.floor(Math.random() * numbers.length);
   return numbers[randomNumber];
 };
 
 const generateRandomSuit = () => {
-  let randomSuit = Math.floor(Math.random() * suits.length);
-  return suits[randomSuit];
+  let randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  for (let x = 0; x < Suits.length; x++) {
+    Suits[x].innerHTML = randomSuit;
+    if (randomSuit == "&#9829;" || randomSuit == "&#9830;")
+      Suits[x].style.color = "red";
+  }
 };
